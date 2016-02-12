b58c482c05373c561d5975a41efd308b87429af1
T.Küchel <devel@zukuul.de>
2016-02-12 13:28:33 +0100

    add more usual tasks for linuxadmin handling

--- handle_linuxadmin.sh
+++ handle_linuxadmin.sh
@@ -3,3 +3,10 @@
 # remove unnecessary files
 cd /home/linuxadmin/
 rm -rf Bilder Dokumente Downloads Musik Öffentlich Vorlagen Videos
+
+su - linuxadmin -c "dbus-launch gsettings set org.mate.screensaver idle-activation-enabled false"
+su - linuxadmin -c "dbus-launch gsettings set org.mate.screensaver lock-enabled false"
+su - linuxadmin -c "dbus-launch gsettings set org.mate.power-manager button-power nothing"
+su - linuxadmin -c "dbus-launch gsettings set org.mate.lockdown disable-user-switching true"
+su - linuxadmin -c "dbus-launch gsettings set org.mate.lockdown disable-lock-screen true"
+
--- workarounds.sh
+++ workarounds.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 # disable blueman applet since it crashes (BUG, 12.02.2016)
-cp dateien/blueman.desktop /home/linuxadmin/.config/autostart
+mkdir -p ~linuxadmin/.config/autostart
+cp dateien/blueman.desktop /home/linuxadmin/.config/autostart/
 chown linuxadmin:linuxadmin /home/linuxadmin/.config/autostart/blueman.desktop
+rm /etc/skel/.config/autostart/ubuntu-mate-welcome.desktop
